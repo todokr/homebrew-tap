f2167a338ac4b46a34857351c520994cfe6b5929
shunsuke.tadokoro <shunsuke.tadokoro@bizreach.co.jp>
2026-07-29 16:37:33 +0900

    tim 0.1.3

--- Formula/timothy.rb
+++ Formula/timothy.rb
@@ -6,9 +6,9 @@
 class Timothy < Formula
   desc "Upload LLM-generated HTML and share via time-limited URLs"
   homepage "https://github.com/todokr/timothy-cli"
-  url "https://github.com/todokr/timothy-cli/releases/download/v0.1.2/tim"
-  sha256 "f31f2c340442717693d792529b5a94e14ce89d967ae24a83359f8c0c82bd9443"
-  version "0.1.2"
+  url "https://github.com/todokr/timothy-cli/releases/download/v0.1.3/tim"
+  sha256 "3cefd2827d7818b7fa837107e65ed76dd792c2a2543a2b918c4a4b9d9b9c45e1"
+  version "0.1.3"
   license "EPL-2.0"
 
   depends_on "node"
